be3a3526e434e7e970c920b4ec216dce175facbb
Léo <69589034+Zomblard@users.noreply.github.com>
2023-07-09 20:24:45 +0200

    [Edit] Cart Controller

--- Controllers/ShopCartsController.php
+++ Controllers/ShopCartsController.php
@@ -4,8 +4,11 @@ namespace CMW\Controller\Shop;
 
 use CMW\Controller\Users\UsersController;
 use CMW\Manager\Package\AbstractController;
+use CMW\Manager\Requests\Request;
 use CMW\Manager\Router\Link;
 use CMW\Manager\Views\View;
+use CMW\Model\Shop\ShopCartsModel;
+use CMW\Model\Users\UsersModel;
 
 
 /**
@@ -20,8 +23,22 @@ class ShopCartsController extends AbstractController
     public function shopCarts(): void
     {
         UsersController::redirectIfNotHavePermissions("core.dashboard", "shop.carts");
-        View::createAdminView('Shop', 'carts')
-            ->addVariableList([])
+        View::createAdminView('Shop', 'Carts/carts')
+            ->view();
+    }
+
+    #[Link("/carts/:userId", Link::GET, [], "/cmw-admin/shop")]
+    public function shopCartUser(Request $request, int $userId): void
+    {
+        UsersController::redirectIfNotHavePermissions("core.dashboard", "shop.carts");
+
+        $carts = ShopCartsModel::getInstance()->getShopCartsByUserId($userId);
+        $user = UsersModel::getInstance()->getUserById($userId);
+
+        View::createAdminView('Shop', 'Carts/userCart')
+            ->addStyle("Admin/Resources/Vendors/Simple-datatables/style.css","Admin/Resources/Assets/Css/Pages/simple-datatables.css")
+            ->addScriptAfter("Admin/Resources/Vendors/Simple-datatables/Umd/simple-datatables.js","Admin/Resources/Assets/Js/Pages/simple-datatables.js")
+            ->addVariableList(["carts" => $carts, "user" => $user])
             ->view();
     }
 }
