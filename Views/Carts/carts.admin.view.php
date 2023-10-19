b156e92b8f123172ff7b6a69103b0c1a59d69dab
Zomblard <leo.goigoux@gmail.com>
2023-10-19 11:59:34 +0200

    [IUsersProfilePicture] getImageLink & getImageName by getImage()

--- Views/Carts/carts.admin.view.php
+++ Views/Carts/carts.admin.view.php
@@ -25,7 +25,7 @@ $description = "";
                         <div class="col-12 col-lg-2">
                             <div href="ss" class="card-in-card p-2">
                                 <div class="text-center">
-                                    <img style="width: 80px; height: 80px" src="<?= EnvManager::getInstance()->getValue("PATH_SUBFOLDER") ?>Public/Uploads/Users/<?= $user->getUserPicture()->getImageName() ?>"
+                                    <img style="width: 80px; height: 80px" src="<?= $user->getUserPicture()->getImage() ?>"
                                          alt="...">
                                 </div>
                                 <h6 class="text-center"><?= $user->getPseudo() ?></h6>
