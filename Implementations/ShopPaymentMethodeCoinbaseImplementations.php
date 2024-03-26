0a678634260a32b4b50054b1b3c2a40cf82ef3bf
Zomb <leo.goigoux@gmail.com>
2024-03-26 16:19:00 +0100

    [ADD] CoinBase cryptocurrencies payment

---
+++ Implementations/ShopPaymentMethodeCoinbaseImplementations.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace CMW\Implementation\Shop;
+
+use CMW\Controller\Shop\Admin\Payment\Method\ShopPaymentMethodCoinbaseController;
+use CMW\Controller\Shop\Admin\Payment\Method\ShopPaymentMethodCryptopayController;
+use CMW\Entity\Shop\Deliveries\ShopDeliveryUserAddressEntity;
+use CMW\Entity\Shop\Deliveries\ShopShippingEntity;
+use CMW\Entity\Users\UserEntity;
+use CMW\Interface\Shop\IPaymentMethod;
+use CMW\Manager\Env\EnvManager;
+use CMW\Model\Shop\Payment\ShopPaymentMethodSettingsModel;
+
+class ShopPaymentMethodeCoinbaseImplementations implements IPaymentMethod
+{
+    public function name(): string
+    {
+        return "CoinBase";
+    }
+
+    public function varName(): string
+    {
+        return str_replace(' ', '_', strtolower($this->name()));
+    }
+
+    public function faIcon(?string $customClass = null): ?string
+    {
+        return "<i class='fa-brands fa-bitcoin $customClass'></i>";
+    }
+
+    public function dashboardURL(): ?string
+    {
+        return "https://beta.commerce.coinbase.com/payments";
+    }
+
+    public function documentationURL(): ?string
+    {
+        return "https://docs.cloud.coinbase.com/commerce-onchain/docs/creating-api-key";
+    }
+
+    public function description(): string
+    {
+        return "TODO DESC I18N";
+    }
+
+    public function fees(): int
+    {
+        return 0; //TODO Var ?
+    }
+
+    public function isActive(): bool
+    {
+        return ShopPaymentMethodSettingsModel::getInstance()->getSetting($this->varName().'_is_active') ?? 0;
+    }
+    public function includeConfigWidgets(): void
+    {
+        require_once EnvManager::getInstance()->getValue("DIR") . "App/Package/Shop/Views/Elements/coinbase.config.inc.view.php";
+    }
+
+    public function doPayment(array $cartItems, UserEntity $user, ShopShippingEntity $shipping, ShopDeliveryUserAddressEntity $address): void
+    {
+        ShopPaymentMethodCoinbaseController::getInstance()->sendCoinbasePayment($cartItems, $shipping, $address);
+    }
+}
--- Views/Elements/coinbase.config.inc.view.php
+++ Views/Elements/coinbase.config.inc.view.php
@@ -1,21 +1,20 @@
 <?php
 
-use CMW\Manager\Env\EnvManager;
 use CMW\Manager\Security\SecurityManager;
 use CMW\Model\Shop\Payment\ShopPaymentMethodSettingsModel;
 
 ?>
 
-<form id="stripe" action="payments/settings" method="post">
+<form id="coinbase" action="payments/settings" method="post">
     <?php (new SecurityManager())->insertHiddenToken(); ?>
     <div class="row">
         <div class="form-group">
-            <label for="stripe_secret_key">Clé Secrète :</label>
-            <input value="<?= ShopPaymentMethodSettingsModel::getInstance()->getSetting('stripe_secret_key') ?>"
+            <label for="coinbase_api_key">Clé Secrète :</label>
+            <input value="<?= ShopPaymentMethodSettingsModel::getInstance()->getSetting('coinbase_api_key') ?>"
                    placeholder="SECRET_KEY"
                    type="text"
-                   name="stripe_secret_key"
-                   id="stripe_secret_key"
+                   name="coinbase_api_key"
+                   id="coinbase_api_key"
                    class="form-control"
                    required>
         </div>
