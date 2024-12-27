fc6a5f8bb2a07e6bf4d7d159eddebd977dd46c69
Léo <69589034+Zomblard@users.noreply.github.com>
2024-12-27 22:14:28 +0100

    [IMPROVE] menu implementation

--- Implementations/Core/ShopMenusImplementations.php
+++ Implementations/Core/ShopMenusImplementations.php
@@ -12,8 +12,15 @@ class ShopMenusImplementations implements IMenus
         $catSlug = [];
         $catSlug['Shop'] = 'shop';
 
+        $catSlug['Panier'] = 'shop/cart';
+
+        $catSlug['Paramètres'] = 'shop/settings';
+
         foreach ((new ShopCategoriesModel())->getShopCategories() as $cat) {
             $catSlug['Cat : ' . $cat->getName()] = 'shop/cat/' . $cat->getSlug();
+            foreach (ShopCategoriesModel::getInstance()->getSubsCat($cat->getId()) as $subcat) {
+                $catSlug['Sous-Cat : ' . $subcat->getSubCategory()->getName()] = 'shop/cat/' . $subcat->getSubCategory()->getSlug();
+            }
         }
 
         return $catSlug;
