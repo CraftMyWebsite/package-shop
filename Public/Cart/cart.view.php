119197f6b3fe33fb7f36143a343286cdd7d5988f
Léo <69589034+Zomblard@users.noreply.github.com>
2025-01-07 14:50:16 +0100

    [IMPROVE] add cart basic public view

--- Public/Cart/cart.view.php
+++ Public/Cart/cart.view.php
@@ -2,7 +2,6 @@
 
 use CMW\Manager\Env\EnvManager;
 use CMW\Manager\Security\SecurityManager;
-use CMW\Model\Core\ThemeModel;
 use CMW\Utils\Website;
 
 /* @var CMW\Entity\Shop\Carts\ShopCartItemEntity[] $cartContent */
@@ -21,15 +20,14 @@ Website::setDescription('Votre panier');
 <section class="shop-section-45875487">
     <h3 style="text-align: center">Panier</h3>
         <div class="shop-grid-main-5548754">
-            <div class="shop-col-span-3-157655">
-
+            <div style="height: fit-content; margin-bottom: 1rem" class="shop-col-span-3-157655 shop-cart-card-45854">
                 <div style="overflow-x: auto; position: relative; height: fit-content">
                     <table style="width: 100%; text-align: left; font-size: .9rem">
                         <thead style="font-weight: bold">
                         <tr>
                             <th style="padding: 0.75rem 1.25rem">
                             </th>
-                            <th style="padding: 0.75rem 1.25rem; text-align: center">
+                            <th style="padding: 0.75rem 1.25rem;">
                                 Produit
                             </th>
                             <th style="padding: 0.75rem 1.25rem; text-align: center">
@@ -99,7 +97,7 @@ Website::setDescription('Votre panier');
                     </table>
                 </div>
 
-                <div style="display: flex; justify-content: end; margin-top: 1rem">
+                <div style="display: flex; justify-content: end;">
                     <div style="width: 30%">
                         <div style="display: flex">
                             <div style="position: relative; width: 100%">
@@ -119,86 +117,59 @@ Website::setDescription('Votre panier');
             </div>
 
 
-            <div style="height: fit-content; margin-top: 1rem">
-                <div style="font-weight: bold; font-size: 1.15rem; text-align: center" >
+            <div class="shop-cart-card-45854" style="height: fit-content">
+                <div style="font-weight: bold; font-size: 1.15rem; text-align: center">
                     <h5>Total panier</h5>
                 </div>
-                <div class="grid grid-cols-2 bg-white">
-                    <div class="font-medium text-center">
-                        <p class="py-2 border-b">Sous total</p>
-                    </div>
-                    <div class="text-center">
-                        <p class="py-2 border-b">
-                            <?= isset($cart) ? $cart->getTotalCartPriceBeforeDiscountFormatted() : 0 ?>
-                        </p>
-                    </div>
+                <div class="shop-grid-cart-total-47875">
+                    <p>Sous total</p>
+                    <p style="font-weight: bolder"><?= isset($cart) ? $cart->getTotalCartPriceBeforeDiscountFormatted() : 0 ?></p>
                 </div>
-                <div class="grid grid-cols-2 bg-white">
-                    <div class="font-medium text-center">
-                        <p class="py-2">Réduction</p>
-                    </div>
+                <div class="shop-grid-cart-total-47875">
+                    <p>Réduction</p>
                 </div>
-                <div class="grid grid-cols-2 bg-white">
+                <div class="shop-grid-cart-total-47875">
                     <?php foreach ($appliedDiscounts as $appliedDiscount): ?>
-                        <div class="text-center mb-2">
+                        <div style="font-weight: bolder">
                             Code : <b><?= $appliedDiscount->getDiscount()->getCode() ?></b>
                         </div>
-                        <div class="text-center">
-                            <b><?= $appliedDiscount->getDiscountFormatted() ?></b>
-                            <a href="<?= $appliedDiscount->getRemoveLink() ?>" class="font-medium ml-4 text-red-600"><i class="fa-solid fa-trash"></i></a>
+                        <div style="display: flex; justify-content: space-between">
+                            <span style="font-weight: bolder"><?= $appliedDiscount->getDiscountFormatted() ?></span>
+                            <a href="<?= $appliedDiscount->getRemoveLink() ?>" style="color: #df3232"><i class="fa-solid fa-trash"></i></a>
                         </div>
                     <?php endforeach; ?>
                 </div>
-                <div class="grid grid-cols-2 bg-white border-t">
-                    <div class="font-medium text-center">
-                        <p class="py-2 ">Total</p>
-                    </div>
-                    <div class="text-center">
-                        <p class="py-2 font-medium">
-                            <?= isset($cart) ? $cart->getTotalCartPriceAfterDiscountFormatted() : 0 ?>
-                        </p>
-                    </div>
+                <div class="shop-grid-cart-total-47875" style="margin-top: 1.6rem">
+                    <p style="font-weight: bolder; font-size: 1.2rem">Total</p>
+                    <p style="font-weight: bolder; font-size: 1.2rem"><?= isset($cart) ? $cart->getTotalCartPriceAfterDiscountFormatted() : 0 ?></p>
                 </div>
-                <a href="command">
-                    <div
-                        class="bg-blue-700 rounded-b-lg text-white hover:bg-blue-800 font-medium text-sm px-2 py-3 text-center">
-                        Commander
-                    </div>
-                </a>
+                <div style="display: flex; justify-content: center">
+                    <a class="shop-button-48751" href="command">Commander</a>
+                </div>
             </div>
         </div>
 
 
     <?php if ($asideCartContent !== []): ?>
-        <section class="bg-white rounded-lg shadow my-8 sm:mx-12 lg:mx-72">
-            <div class="container p-4">
-                <h4 class="text-center mb-2">Article mis de côté</h4>
-                <div class="overflow-x-auto relative shadow-md sm:rounded-lg h-fit">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="font-medium text-gray-700 bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+    <h3 style="text-align: center; margin-top: 3rem">Article mis de côté</h4>
+        <section class="shop-cart-card-45854">
+                <div style="overflow-x: auto; position: relative; height: fit-content">
+                    <table style="width: 100%; text-align: left; font-size: .9rem">
+                        <thead style="font-weight: bold">
                         <tr>
-                            <th class="py-3 px-6">
-                            </th>
-                            <th class="text-center py-3 px-6">
-                                Produit
-                            </th>
+                            <th style="padding: 0.75rem 1.25rem"></th>
+                            <th style="padding: 0.75rem 1.25rem">Produit</th>
                             <?php if ($showPublicStock): ?>
-                                <th class="text-center py-3 px-6">
-                                    Stock restant
-                                </th>
+                                <th style="padding: 0.75rem 1.25rem; text-align: center">Stock restant</th>
                             <?php endif ?>
-                            <th class="text-center py-3 px-6">
-                                Prix
-                            </th>
-                            <th class="py-3 px-6">
-
-                            </th>
+                            <th style="padding: 0.75rem 1.25rem; text-align: center">Prix</th>
+                            <th style="padding: 0.75rem 1.25rem"></th>
                         </tr>
                         </thead>
-                        <tbody class="text-center">
+                        <tbody>
                         <?php foreach ($asideCartContent as $asideCart): ?>
-                            <tr class="bg-white border-b text-center">
-                                <td class="py-2">
+                            <tr>
+                                <td style="padding: 0.75rem 1.25rem">
                                     <?php if ($asideCart->getFirstImageItemUrl() !== '/Public/Uploads/Shop/0'): ?>
                                         <img class="mx-auto" style="width: 3rem; height: 3rem; object-fit: cover"
                                              src="<?= $asideCart->getFirstImageItemUrl() ?>" alt="Panier">
@@ -207,21 +178,21 @@ Website::setDescription('Votre panier');
                                              src="<?= $defaultImage ?>" alt="Panier">
                                     <?php endif; ?>
                                 </td>
-                                <td class="py-4 px-6 font-semibold text-gray-900">
+                                <td style="padding: 0.75rem 1.25rem">
                                     <?= $asideCart->getItem()->getName() ?>
                                 </td>
                                 <?php if ($showPublicStock): ?>
-                                    <td class="py-4 px-6 text-center">
+                                    <td style="padding: 0.75rem 1.25rem; text-align: center">
                                         <?= $asideCart->getItem()->getPublicFormattedStock() ?>
                                     </td>
                                 <?php endif ?>
-                                <td class="py-4 px-6 text-gray-900">
+                                <td style="padding: 0.75rem 1.25rem; text-align: center">
                                     <?= $asideCart->getItem()->getPriceFormatted() ?>
                                 </td>
-                                <td>
-                                    <a href="<?= $asideCart->getUnAsideLink() ?>" class="mr-4 font-medium text-blue-700">
+                                <td style="padding: 0.75rem 1.25rem; text-align: center">
+                                    <a href="<?= $asideCart->getUnAsideLink() ?>" style="color: #0a58ca; margin-right: .6rem">
                                         <i class="fa-solid fa-cart-arrow-down"></i></a>
-                                    <a href="<?= $asideCart->getRemoveLink() ?>" class="font-medium ml-4 text-red-600"><i
+                                    <a href="<?= $asideCart->getRemoveLink() ?>" style="color: #cf3434"><i
                                             class="fa-solid fa-trash"></i></a>
                                 </td>
                             </tr>
@@ -229,7 +200,6 @@ Website::setDescription('Votre panier');
                         </tbody>
                     </table>
                 </div>
-            </div>
         </section>
     <?php endif; ?>
 </section>
