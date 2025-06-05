0bd37ac22d7647c1f007b16120d49bf3fbe0936a
Léo <69589034+Zomblard@users.noreply.github.com>
2025-06-05 11:03:52 +0200

    [ADD] Version

--- Package.php
+++ Package.php
@@ -16,7 +16,7 @@ class Package implements IPackageConfig
 
     public function version(): string
     {
-        return '0.0.1';
+        return '0.0.2';
     }
 
     public function authors(): array
